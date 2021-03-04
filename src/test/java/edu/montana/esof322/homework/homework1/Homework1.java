c2c0f2324e587ca4edd719bead5f2e3be342e100
joshanderson6 <josh.andersonn94@gmail.com>
2021-03-03 17:38:06 -0700

    java hw completed

--- src/test/java/edu/montana/esof322/homework/homework1/Homework1.java
+++ src/test/java/edu/montana/esof322/homework/homework1/Homework1.java
@@ -7,8 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Homework1 {
 
     @Test
-    void fixThisTest() {
-        assertTrue(false);
+    void fixThisTest() { assertFalse(false);
     }
 
     @Test
@@ -16,16 +15,14 @@ public class Homework1 {
 
         // add the code to work with this builder
         // please chain the methods
-        Student s = Student.builder()
-                .build();
+        Student s = Student.builder().withFirstName("Joe").withLastName("Smith").withId("An ID").build();
 
         assertEquals("Joe", s.getFirstName());
         assertEquals("Smith", s.getLastName());
         assertEquals("An ID", s.getId());
 
         // build a student and take advantage of the autoid generation
-        Student s2 = Student.builder()
-                .build();
+        Student s2 = Student.builder().withFirstName("Joe").withLastName("Smith").build();
 
         assertEquals("Joe", s2.getFirstName());
         assertEquals("Smith", s2.getLastName());
@@ -35,11 +32,8 @@ public class Homework1 {
     @Test
     void createABuilder() {
         // TODO - uncomment and make this builder work
-        Ski ski = Ski.builder()
-                //.withLength(180)
-                //.withBrand("dps")
-                //.withName("Alchemist Lotus")
-                    .build();
+        Ski ski = Ski.builder().withLength(180).withBrand("dps").withName("Alchemist Lotus").build();
+
         assertEquals("dps", ski.getBrand());
         assertEquals("Alchemist Lotus", ski.getName());
         assertEquals(180, ski.getLength());
